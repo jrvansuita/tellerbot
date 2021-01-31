6b03e2a589e9970bc384bfba1b17d36e2bf2e9b1
juniorvansuita <Vansuita1324>
2021-01-31 18:10:03 -0300

    scheduler

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const schedule = require('node-schedule');
+
 const Searcher = require('./shops/searcher.js');
 const MercadoLivreCalls = require('./shops/mercado-livre/calls.js');
 const OlxCalls = require('./shops/olx/calls.js');
@@ -8,18 +10,11 @@ var searcher = new Searcher();
 var ml = new MercadoLivreCalls();
 var olx = new OlxCalls();
 
-searcher
-    .find(ml.gpus().mobos().get())
-    .find(olx.gpus().get())
-    .go()
 
+schedule.scheduleJob('*/15 * * * *', () => {
 
-
-
-
-
-//----------------------
-// var schedule = require('node-schedule');
-
-// //Run every 1 min
-// var j = schedule.scheduleJob('*/1 * * * *', scrapeMl);
+    searcher
+        .find(ml.gpus().mobos().get())
+        .find(olx.gpus().get())
+        .go()
+});
