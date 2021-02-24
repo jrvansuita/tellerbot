ba6e2fbc2520a3dfd7df97678cb5b4ed13393c65
juniorvansuita <Vansuita1324>
2021-02-24 09:14:59 -0300

    hours

--- scheduler/job.js
+++ scheduler/job.js
@@ -1,30 +1,42 @@
+
 const schedule = require('node-schedule');
 const Prefs = require('../redis/prefs.js');
 
 let mainJob;
 
+const HOURS_START = 7;
+const HOURS_END = 23;
+
 module.exports = class Job {
 
 
     async buildPattern() {
-        var result = '';
+        var pattern = '';
         var prefValue = await Prefs.huntsEvery();
         var time = prefValue.match(/\d+/g);
 
-        if (prefValue.toLowerCase().includes('h')) result = '* */' + time;
+        if (prefValue.toLowerCase().includes('h')) pattern = '* */' + time;
 
-        if (prefValue.toLowerCase().includes('m')) result = '*/' + time + ' *';
+        if (prefValue.toLowerCase().includes('m')) pattern = '*/' + time + ' *';
 
-        result = result + ' * * *';
+        pattern = pattern + ' * * *';
 
-        //console.log(result)
+        const startTime = new Date()
+        startTime.setHours(HOURS_START)
+        startTime.setMinutes(0)
+
+        const endTime = new Date();
+        endTime.setHours(HOURS_END);
+        endTime.setMinutes(0)
+
+        var result = { start: startTime, end: endTime, rule: pattern };
+        console.log(result);
+
         return result;
     }
 
     static async reschedule() {
         mainJob.reschedule(await new Job().buildPattern())
-
-        //console.log(mainJob.nextInvocation())
     }
 
     static now() {
